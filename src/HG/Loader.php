a592f1f3da67b91b1a786a2cb276e1e3795fa43d
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-12-29 13:21:59 -0200

    Update Loader.php

--- src/HG/Loader.php
+++ src/HG/Loader.php
@@ -5,8 +5,12 @@ namespace HG;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
+use HG\ArenaModel\Arena;
+use HG\ArenaModel\ArenaManager;
+
 class Loader extends PluginBase implements Listener{
-
-
-
+  
+  public function onEnable(){
+  }
+  
 }
