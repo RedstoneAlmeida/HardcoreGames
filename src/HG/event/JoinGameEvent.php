f84a72f12e0340adab41705f0ff71bc833e095d9
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-12-29 18:39:40 -0200

    Update JoinGameEvent.php

--- src/HG/event/JoinGameEvent.php
+++ src/HG/event/JoinGameEvent.php
@@ -5,6 +5,10 @@ namespace HG\event;
 use pocketmine\event\Cancellable;
 use pocketmine\event\player\PlayerEvent;
 
+use HG\ArenaModel\Arena;
+
+use pocketmine\Player;
+
 class JoinGameEvent extends PlayerEvent implements Cancellable{
   
     public $player;
